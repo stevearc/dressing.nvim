8c1500069cafc9cfd93bf5521b17fde060008022
Steven Arcangeli <stevearc@stevearc.com>
2022-11-09 03:17:33 +0000

    ci: fix nvim appimage installation

--- .github/workflows/install_nvim.sh
+++ .github/workflows/install_nvim.sh
@@ -5,4 +5,8 @@ mkdir -p "$PLUGINS"
 
 wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage
 chmod +x nvim.appimage
-sudo mv ./nvim.appimage /usr/bin/nvim
+./nvim.appimage --appimage-extract >/dev/null
+rm -f nvim.appimage
+mkdir -p ~/.local/share/nvim
+mv squashfs-root ~/.local/share/nvim/appimage
+sudo ln -s "$HOME/.local/share/nvim/appimage/AppRun" /usr/bin/nvim
