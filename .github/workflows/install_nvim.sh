2319ee23974edab69d68623ec56225113706325d
Steven Arcangeli <stevearc@stevearc.com>
2022-11-09 01:35:29 +0000

    ci: improve sandboxing of tests

--- .github/workflows/install_nvim.sh
+++ .github/workflows/install_nvim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -e
 PLUGINS="$HOME/.local/share/nvim/site/pack/plugins/start"
 mkdir -p "$PLUGINS"
 
 wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage
 chmod +x nvim.appimage
 sudo mv ./nvim.appimage /usr/bin/nvim
-git clone --depth=1 https://github.com/nvim-lua/plenary.nvim.git "$PLUGINS/plenary.nvim"
