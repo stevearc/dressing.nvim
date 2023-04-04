324f8f16e0743fe09735d77d4aeb538d28ee30cc
Steven Arcangeli <stevearc@stevearc.com>
2023-04-04 10:54:52 -0700

    feat: provide better default window options for vim.ui.input (#94)

--- lua/dressing/config.lua
+++ lua/dressing/config.lua
@@ -35,6 +35,11 @@ local default_config = {
       winblend = 10,
       -- Disable line wrapping
       wrap = false,
+      -- Indicator for when text exceeds window
+      list = true,
+      listchars = "precedes:…,extends:…",
+      -- Increase this for more context when text scrolls off the window
+      sidescrolloff = 0,
     },
 
     -- Set to `false` to disable
